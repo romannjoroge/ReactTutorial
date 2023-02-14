5e963cbfadf8a4ecdd807c1a40b3660c08d9f4c3
romannjoroge <roman.njoroge@njuguna.com>
2023-02-15 01:20:18 +0300

    Handling click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 function Home(){
+
+    function clickEvent(){
+        console.log("I'm ticklish stop clicking me");
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={clickEvent}>Don't Click Me</button>
         </div>
     );
 }
