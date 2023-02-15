7c601993ec3ee42b9e5ba9fd65f7c862c02e5d59
romannjoroge <roman.njoroge@njuguna.com>
2023-02-15 10:53:06 +0300

    Working with lists of data

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 function Home(){
     const [meal, setMeal] = useState('Choose A Meal');
+    const [blogs, setBlogs] = useState(
+        [
+            {title: 'My New Website', body: 'lorem ipsum...', author: 'Roman', id: 1},
+            {title: 'My Formulations Guide', body: 'lorem ipsum...', author: 'Caroline', id: 2},
+            {title: 'Dancing Salsa In the Stars', body: 'lorem ipsum...', author: 'Patrick', id: 3},
+            {title: 'Getting The Kill: God of War Edition', body: 'lorem ipsum...', author: 'Rico', id: 4},
+        ]
+    );
 
     function clickEvent(){
         console.log("I'm ticklish stop clicking me");
@@ -26,6 +34,15 @@ function Home(){
             <h2>Get a New Meal</h2>
             <p>{ meal }</p>
             <button onClick={ changeMeal }>Get Meal</button>
+            <h2>Blogs</h2>
+            <div className="blogs">
+                { blogs.map((blog) => (
+                    <div className="blog-preview" key = {blog.id}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written By: { blog.author }</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
